12c66fb70bbedfd1b4936e32a6eee7739722dd61
Rajalakshmy Iyer <supriya.iyer@hotmail.com>
2016-03-20 20:15:48 -0400

    SetUp first polygon

--- src/CatanGameBoard.java
+++ src/CatanGameBoard.java
@@ -4,21 +4,30 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class CatanGameBoard extends JPanel {
+public class CatanGameBoard extends JPanel { 
+    static Dimension screenSize;  
 
-	public void baseBoard(Graphics g) {
+	public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        Polygon p1 = new Polygon();
+
+        for (int i = 0; i < 6; i++) {
+            p1.addPoint((int) (screenSize.width/2 + 50 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2- 20) + 50 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+
+    g.drawPolygon(p1);
+
+
 	}
 
-	private static void makeFrameFullSize(JFrame aFrame){
- 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
- 		aFrame.setSize(screenSize.width, screenSize.height);
-	}
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setTitle("Catan");
-        makeFrameFullSize(frame);
-        //frame.setSize(1000, 1000);
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width, screenSize.height);
         frame.setLocationRelativeTo(null);
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -27,6 +36,7 @@ public class CatanGameBoard extends JPanel {
         });
         Container contentPane = frame.getContentPane();
         contentPane.add(new CatanGameBoard());
+
 
         frame.show();
     }
