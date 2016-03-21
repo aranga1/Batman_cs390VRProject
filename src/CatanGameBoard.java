6fb6c65c1934dc0e0692c93ffff78fbd9821ff59
Rajalakshmy Iyer <supriya.iyer@hotmail.com>
2016-03-20 21:01:19 -0400

    made game board

--- src/CatanGameBoard.java
+++ src/CatanGameBoard.java
@@ -9,15 +9,145 @@ public class CatanGameBoard extends JPanel {
 
 	public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        //g.translate( screenSize.width/2, screenSize.height/2);
         
         Polygon p1 = new Polygon();
-
         for (int i = 0; i < 6; i++) {
-            p1.addPoint((int) (screenSize.width/2 + 50 * Math.cos(i * 2 * Math.PI / 6)),
-          (int) ((screenSize.height/2- 20) + 50 * Math.sin(i * 2 * Math.PI / 6)));
+            p1.addPoint((int) (screenSize.width/2 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+        g.drawPolygon(p1);
+
+        Polygon p2 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p2.addPoint((int) ((screenSize.width/2 + 150) + 100 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2 + 100*Math.sin(Math.PI / 3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+        g.drawPolygon(p2);
+
+        Polygon p3 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p3.addPoint((int) ((screenSize.width/2 - 150) + 100 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2 - 100*Math.sin(Math.PI / 3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+        g.drawPolygon(p3);
+
+        Polygon p4 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p4.addPoint((int) ((screenSize.width/2 - 150) + 100 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2 + 100*Math.sin(Math.PI / 3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+        g.drawPolygon(p4);
+
+        Polygon p5 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p5.addPoint((int) ((screenSize.width/2 + 150) + 100 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2 - 100*Math.sin(Math.PI / 3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+        g.drawPolygon(p5);
+
+        Polygon p6 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p6.addPoint((int) (screenSize.width/2 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2 + 200*Math.sin(Math.PI / 3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+        g.drawPolygon(p6);
+
+        Polygon p7 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p7.addPoint((int) (screenSize.width/2 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+          (int) ((screenSize.height/2 - 200*Math.sin(Math.PI / 3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+        g.drawPolygon(p7);
+
+        Polygon p8 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p8.addPoint((int) ((screenSize.width/2 + 300) + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) (screenSize.height/2 + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p8);
+
+        Polygon p9 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p9.addPoint((int) ((screenSize.width/2 - 150) - 150 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) (screenSize.height/2 + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p9);
+
+        Polygon p10 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p10.addPoint((int) (screenSize.width/2 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 - 4 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
         }
 
-    g.drawPolygon(p1);
+        g.drawPolygon(p10);
+
+        Polygon p11 = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            p11.addPoint((int) (screenSize.width/2 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 + 4 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+        }
+
+        g.drawPolygon(p11);
+
+        Polygon p12 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p12.addPoint((int) (screenSize.width/2 + 300 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 + 2 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p12);
+
+        Polygon p13 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p13.addPoint((int) (screenSize.width/2 + 300 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 - 2 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p13);
+
+        Polygon p14 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p14.addPoint((int) ((screenSize.width/2 - 150) - 150 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 + 2 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p14);
+
+        Polygon p15 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p15.addPoint((int) ((screenSize.width/2 - 150) - 150 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 - 2 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p15);
+
+        Polygon p16 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p16.addPoint((int) (screenSize.width/2 + 150 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 + 3 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p16);
+
+        Polygon p17 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p17.addPoint((int) (screenSize.width/2 + 150 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 - 3 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+       g.drawPolygon(p17);
+
+        Polygon p18 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p18.addPoint((int) (screenSize.width/2 -150 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 + 3 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p18);
+
+        Polygon p19 = new Polygon();
+        for (int i = 0; i < 6; i++)
+            p19.addPoint((int) (screenSize.width/2 -150 + 100 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) ((screenSize.height/2 - 3 * 50 * Math.sqrt(3)) + 100 * Math.sin(i * 2 * Math.PI / 6)));
+
+        g.drawPolygon(p19);
+
+        
 
 
 	}
